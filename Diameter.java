9a7c05fb8ea754917559cae4968a69a405efd796
maozlev <73070711+maozlev@users.noreply.github.com>
2021-06-05 23:06:57 +0300

    2 Algorithms to find Diameter Bfs and Dfs

--- Diameter.java
+++ Diameter.java
@@ -1,32 +1,46 @@
+
 import java.util.*;
 import java.lang.*;
 
 //ID: 204594758
+
+/**
+ find the diameter of graph
+ diameter is the greatest distance between nodes
+ input - matrix of neighbors
+ output - diameter
+ */
+
 public class Diameter {
-
-    private boolean [][] mat;
-    private final int n;
-    static int x;
-    static int maxCount;
-    private final List<Integer>[] adj;
-
-    public Diameter(boolean[][] adj_matrix) {
-        n = Math.max(adj_matrix.length, adj_matrix[0].length);
-        adj = new List[n];
-        for (int i = 0; i < adj.length ; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < adj_matrix.length; i++) {
-            for (int j = 0; j <adj_matrix[0].length ; j++) {
-                if(adj_matrix[i][j]){
-                    adj[i].add(j);
-                }
-            }
-        }
-    }
 
     public int get_diam() {
         return diameter(adj, n)-1;
+    }
+
+    static int diameter(List<Integer>[] adj, int n) {
+        maxCount = Integer.MIN_VALUE;
+
+        /* DFS from a random node and then see
+        farthest node X from it*/
+        dfs(1, n, adj);
+
+        /* DFS from X and check the farthest node
+        from it */
+        dfs(x, n, adj);
+
+        return maxCount;
+    }
+
+    static void dfs(int node, int n, List<Integer>[] adj) {
+        boolean[] visited = new boolean[n + 1];
+        int count = 0;
+
+        // Mark all the vertices as not visited
+        Arrays.fill(visited, false);
+
+        // Increment count by 1 for visited node
+        dfsUtil(node, count + 1, visited, adj);
+
     }
 
     static void dfsUtil(int node, int count, boolean[] visited, List<Integer>[] adj) {
@@ -44,89 +58,168 @@ public class Diameter {
         }
     }
 
-    static void dfs(int node, int n, List<Integer>[] adj) {
-        boolean[] visited = new boolean[n + 1];
-        int count = 0;
+    private static int n = 0;
+    static int x;
+    static int maxCount;
+    static LinkedList<Integer>[] adj;
+    static int[] diameter1, visited;
+    static Queue<Integer> q;
 
-        // Mark all the vertices as not visited
-        Arrays.fill(visited, false);
-
-        // Increment count by 1 for visited node
-        dfsUtil(node, count + 1, visited, adj);
-
+    public Diameter(boolean[][] adj_matrix) {
+        n = Math.max(adj_matrix.length, adj_matrix[0].length);
+        diameter1 = new int[n];
+        visited = new int[n];
+        adj = new LinkedList[n];
+        q = new LinkedList<>();
+        int i = 0;
+        while (i < adj.length ) {
+            adj[i] = new LinkedList<>();
+            i++;
+        }
+        i=0;
+        int j = -1;
+        while( i < n) {
+            j=0;
+            while(j < n) {
+                if(adj_matrix[i][j]){
+                    adj[i].add(j);
+                }
+                j++;
+            }
+            i++;
+        }
     }
 
-    static int diameter(List<Integer>[] adj, int n) {
-        maxCount = Integer.MIN_VALUE;
+    public int findDiameter() {
+        int init = bfs(1);
+        q.clear();
+        int i=0;
+        while(i <= n-1){
+            visited[i] = 0;
+            diameter1[i] = 0;
+            i++;
+        }
+        int val = bfs(init);
+        return diameter1[val];
+    }
 
-        /* DFS from a random node and then see
-        farthest node X from it*/
-        dfs(1, n, adj);
+    /**
+     The Function to do bfs traversal.
+     It uses iterative approach to do bfs
+     */
 
-        /* DFS from X and check the farthest node
-        from it */
-        dfs(x, n, adj);
+    static int bfs(int init) {
+        // Pushing each node in queue
+        q.add(init);
 
-        return maxCount;
+        // Mark the traversed node visited
+        visited[init] = 1;
+        while (!q.isEmpty()) {
+            int u = q.peek();
+            q.remove();
+            int i = 0;
+            while( i < adj[u].size()) {
+                if (visited[adj[u].get(i)] == 0) {
+                    visited[adj[u].get(i)] = 1;
+                    // Considering weight of edges equal to 1
+                    diameter1[adj[u].get(i)] += diameter1[u] + 1;
+                    q.add(adj[u].get(i));
+                }
+                i++;
+            }
+        }
+
+        int in = 0;
+        int i=0;
+        while( i <= n-1) {
+            if(diameter1[i] > diameter1[in])
+                in = i;
+            i++;
+        }
+
+        // return index of max value in diameter
+        return in;
     }
 
     public static void main(String[] args) {
-            boolean[][] matrixTest1 = {
-                    {false, true, false, false, false, false},
-                    {true, false, true, true, false, false},
-                    {false, true, false, false, false, false},
-                    {false, true, false, false, true, true},
-                    {false, false, false, true, false, false},
-                    {false, false, false, true, false, false}
-            };
+        boolean[][] matrixTest1 = {
+                {false, true, false, false, false, false},
+                {true, false, true, true, false, false},
+                {false, true, false, false, false, false},
+                {false, true, false, false, true, true},
+                {false, false, false, true, false, false},
+                {false, false, false, true, false, false}
+        };
 
-            Diameter l = new Diameter(matrixTest1);
-            long start = System.nanoTime();
-            System.out.println("Test 1: " + ((l.get_diam() == 3) ? "Pass." : "Fail."));
-            long end = System.nanoTime();
-            System.out.println(end-start);
-            boolean t = true, f = false;
-            boolean[][] matrixTest2 = {
-                    {false, false, false, true, false, false, true, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, false, true, false, false},
-                    {false, false, false, false, false, false, false, false, true, false, false, true},
-                    {true, false, false, false, false, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, true, false, false, true, true, false, false},
-                    {false, false, false, false, true, false, true, true, false, false, false, false},
-                    {true, false, false, false, false, true, false, false, false, false, false, false},
-                    {false, false, false, false, false, true, false, false, false, false, false, false},
-                    {false, false, true, false, true, false, false, false, false, false, true, false},
-                    {false, true, false, false, true, false, false, false, false, false, false, false},
-                    {false, false, false, false, false, false, false, false, true, false, false, false},
-                    {false, false, true, false, false, false, false, false, false, false, false, false}};
+        long start1 = System.nanoTime();
+        Diameter l1 = new Diameter(matrixTest1);
+        System.out.println("Test 1: " + ((l1.get_diam() == 3) ? "Pass." : "Fail."));
+        long end1 = System.nanoTime();
+        System.out.println("time DFS recurs: "+(end1-start1));
 
-            Diameter d = new Diameter(matrixTest2);
-            long start1 = System.nanoTime();
-            System.out.println("Test 2: " + ((d.get_diam() == 7) ? "Pass." : "Fail."));
-            long end1 = System.nanoTime();
-            System.out.println(end1-start1);
-            boolean[][] matrixTest3 = {{f, t, f, f, f, f, t, f, f, f, f, f, f, f, f, f, f},
-                    {t, f, f, f, f, t, f, f, f, f, f, t, f, f, f, f, f},
-                    {f, f, f, f, t, f, f, f, f, t, f, f, f, f, f, f, f},
-                    {f, f, f, f, t, f, f, t, f, f, f, f, f, f, f, f, f},
-                    {f, f, t, t, f, t, f, f, f, f, f, f, f, f, f, f, f},
-                    {f, t, f, f, t, f, f, f, f, f, f, f, f, f, f, f, f},
-                    {t, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f},
-                    {f, f, f, t, f, f, f, f, f, f, f, f, t, t, f, f, f},
-                    {f, f, f, f, f, f, f, f, f, f, f, t, f, f, f, f, f},
-                    {f, f, t, f, f, f, f, f, f, f, f, f, f, f, f, f, f},
-                    {f, f, f, f, f, f, f, f, f, f, f, t, f, f, f, f, f},
-                    {f, t, f, f, f, f, f, f, t, f, t, f, f, f, f, f, f},
-                    {f, f, f, f, f, f, f, t, f, f, f, f, f, f, f, f, f},
-                    {f, f, f, f, f, f, f, t, f, f, f, f, f, f, t, f, f},
-                    {f, f, f, f, f, f, f, f, f, f, f, f, f, t, f, t, f},
-                    {f, f, f, f, f, f, f, f, f, f, f, f, f, f, t, f, t},
-                    {f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, t, f}};
-            Diameter p = new Diameter(matrixTest3);
-            long start2 = System.nanoTime();
-            System.out.println("Test 3: " + ((p.get_diam() == 10) ? "Pass." : "Fail."));
-            long end2 = System.nanoTime();
-            System.out.println(end2-start2);
+        long start = System.nanoTime();
+        Diameter l = new Diameter(matrixTest1);
+        System.out.println("Test 1: " + ((l.findDiameter() == 3) ? "Pass." : "Fail."));
+        long end = System.nanoTime();
+        System.out.println("time bfs iterative: "+(end-start));
+
+        boolean t = true, f = false;
+        boolean[][] matrixTest2 = {
+                {false, false, false, true, false, false, true, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, true, false, false},
+                {false, false, false, false, false, false, false, false, true, false, false, true},
+                {true, false, false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, true, false, false, true, true, false, false},
+                {false, false, false, false, true, false, true, true, false, false, false, false},
+                {true, false, false, false, false, true, false, false, false, false, false, false},
+                {false, false, false, false, false, true, false, false, false, false, false, false},
+                {false, false, true, false, true, false, false, false, false, false, true, false},
+                {false, true, false, false, true, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, true, false, false, false},
+                {false, false, true, false, false, false, false, false, false, false, false, false}};
+
+        long start2 = System.nanoTime();
+        Diameter d = new Diameter(matrixTest2);
+        System.out.println("Test 2: " + ((d.get_diam() == 7) ? "Pass." : "Fail."));
+        long end2 = System.nanoTime();
+        System.out.println("Dfs recurs time: "+(end2-start2));
+
+        long start3 = System.nanoTime();
+        Diameter d1 = new Diameter(matrixTest2);
+        System.out.println("Test 2: " + ((d1.findDiameter() == 7) ? "Pass." : "Fail."));
+        long end3 = System.nanoTime();
+        System.out.println("Bfs iterative time: "+(end3-start3));
+
+        boolean[][] matrixTest3 = {
+                {f, t, f, f, f, f, t, f, f, f, f, f, f, f, f, f, f},
+                {t, f, f, f, f, t, f, f, f, f, f, t, f, f, f, f, f},
+                {f, f, f, f, t, f, f, f, f, t, f, f, f, f, f, f, f},
+                {f, f, f, f, t, f, f, t, f, f, f, f, f, f, f, f, f},
+                {f, f, t, t, f, t, f, f, f, f, f, f, f, f, f, f, f},
+                {f, t, f, f, t, f, f, f, f, f, f, f, f, f, f, f, f},
+                {t, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, f},
+                {f, f, f, t, f, f, f, f, f, f, f, f, t, t, f, f, f},
+                {f, f, f, f, f, f, f, f, f, f, f, t, f, f, f, f, f},
+                {f, f, t, f, f, f, f, f, f, f, f, f, f, f, f, f, f},
+                {f, f, f, f, f, f, f, f, f, f, f, t, f, f, f, f, f},
+                {f, t, f, f, f, f, f, f, t, f, t, f, f, f, f, f, f},
+                {f, f, f, f, f, f, f, t, f, f, f, f, f, f, f, f, f},
+                {f, f, f, f, f, f, f, t, f, f, f, f, f, f, t, f, f},
+                {f, f, f, f, f, f, f, f, f, f, f, f, f, t, f, t, f},
+                {f, f, f, f, f, f, f, f, f, f, f, f, f, f, t, f, t},
+                {f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, t, f}};
+
+        long start4 = System.nanoTime();
+        Diameter p = new Diameter(matrixTest3);
+        System.out.println("Test 3: " + ((p.get_diam() == 10) ? "Pass." : "Fail."));
+        long end4 = System.nanoTime();
+        System.out.println("Dfs recurs time: "+(end4-start4));
+
+        long start5 = System.nanoTime();
+        Diameter p1 = new Diameter(matrixTest3);
+        System.out.println("Test 3: " + ((p1.findDiameter() == 10) ? "Pass." : "Fail."));
+        long end5 = System.nanoTime();
+        System.out.println("Bfs iterative time: "+(end5-start5));
 
 
         boolean[][]matrix2 = {
@@ -143,11 +236,17 @@ public class Diameter {
                 {f,t,f,f,t,f,f,f,f,f,f,f},//9
                 {f,f,f,f,f,f,f,f,t,f,f,f},//10
                 {f,f,t,f,f,f,f,f,f,f,f,f}};//11
-        long start4 = System.nanoTime();
-        Diameter D= new Diameter(matrix2);
-        int res = D.get_diam();
-        System.out.println("ans = "+res);//ans = 7
-        System.out.println("time = "+(System.nanoTime()-start));
-        }
-    }
 
+        long start6 = System.nanoTime();
+        Diameter P = new Diameter(matrix2);
+        System.out.println("Test 4: " + ((P.get_diam() == 7) ? "Pass." : "Fail."));
+        long end6 = System.nanoTime();
+        System.out.println("Dfs recurs time: "+(end6-start6));
+
+        long start7 = System.nanoTime();
+        Diameter P1 = new Diameter(matrix2);
+        System.out.println("Test 4: " + ((P1.findDiameter() == 7) ? "Pass." : "Fail."));
+        long end7 = System.nanoTime();
+        System.out.println("Bfs iterative time: "+(end7-start7));
+    }
+}
